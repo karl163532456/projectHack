a264f89a2ab6863baa3b41ae2300c2ebb07f1e2c
karl163532456 <balabanovvladislav00@gmail.com>
2023-06-08 14:13:45 +0400

    Initial commit

---
+++ src/test/java/com/example/projecthack/ProjectHackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projecthack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectHackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
